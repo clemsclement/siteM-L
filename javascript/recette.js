eb9b9b54c5f994342727b1bc34a75fee51f50bfc
clemsclement <clemsclement@hotmail.fr>
2024-06-19 10:55:44 +0200

    Fin ? Recette

--- javascript/recette.js
+++ javascript/recette.js
@@ -1,82 +1,14 @@
-// //Récupération des éléments de la recette Saumon
-
-// const moinsSaumon = document.querySelector("#moinsSaumon");
-// const plusSaumon = document.querySelector("#plusSaumon");
-// const chiffreSaumon = document.querySelector("#chiffreSaumon");
-// const personneSaumon = document.querySelector("#personneSaumon");
-// const ingSaumonSaumon = document.querySelector("#ingSaumonSaumon");
-// const ingPoivreSaumon = document.querySelector("#ingPoivreSaumon");
-// const ingAnethSaumon = document.querySelector("#ingAnethSaumon");
-// const ingSucreSaumon = document.querySelector("#ingSucreSaumon");
-// const ingSelSaumon = document.querySelector("#ingSelSaumon");
-
-
-// //Ajout du mot "personne" au <span> personneSaumon
-
-// personneSaumon.textContent = "personne";
-
-
-// //Mise en place de plusieurs compteurs
-
-// let compteur = 1;
-
-// const ingSaumon = {
-//     saumon: 140,
-//     poivre: 4,
-//     aneth: 10,
-//     sucre: 18,
-//     sel: 18
-// };
-
-
-
-// // Fonction pour mettre à jour les quantités
-
-// function majQuantite() {
-//     ingSaumonSaumon.textContent = ingSaumon.saumon * compteur;
-//     ingPoivreSaumon.textContent = ingSaumon.poivre * compteur;
-//     ingAnethSaumon.textContent = ingSaumon.aneth * compteur;
-//     ingSucreSaumon.textContent = ingSaumon.sucre * compteur;
-//     ingSelSaumon.textContent = ingSaumon.sel * compteur;
-// }
-
-// majQuantite();
-
-
-// //Ajout d'un compteur de base
-
-// chiffreSaumon.textContent = compteur;
-
-
-// //Ecouteur d'évenement au click pour changer les quantités
-
-// plusSaumon.addEventListener("click", () => {
-//     compteur ++;
-//     chiffreSaumon.textContent = compteur;
-//     majQuantite();
-//     if(compteur > 1){
-//         personneSaumon.textContent = "personnes";
-//     }
-// });
-
-// moinsSaumon.addEventListener("click", () => {
-//     if(compteur > 1){
-//         compteur --;
-//         chiffreSaumon.textContent = compteur;
-//         majQuantite();
-//     }
-//     if(compteur == 1){
-//         personneSaumon.textContent = "personne";
-//     }
-// });
+// Définition de la classe Recette avec un nbPersonne de base de 1
 
 class Recette {
-    constructor(nbPersonne = 1, nomRecette, listeIngredient = []) {
-        this.nbPersonne = nbPersonne;
+    constructor(nomRecette, listeIngredient = [], nbPersonne = 1) {
         this.nomRecette = nomRecette;
         this.listeIngredient = listeIngredient;
+        this.nbPersonne = nbPersonne;
     }
 };
+
+// Définition de la classe Ingredient pour le constructeur listeIngredient de la classe Recette
 
 class Ingredient {
     constructor(nom, quantite) {
@@ -86,8 +18,11 @@ class Ingredient {
 
 };
 
-let r1 = new Recette(
-    1,
+
+
+// #region Recette Saumon
+
+let recetteSaumon = new Recette(
     "Saumon Gravlax",
     [new Ingredient("Saumon Frais", 140),
     new Ingredient("Poivre Concassé", 4),
@@ -96,50 +31,224 @@ let r1 = new Recette(
     new Ingredient("Sel", 18)]
 );
 
-const titrer1 = document.getElementById("titre");
-titrer1.textContent = r1.nomRecette;
+const titreRecetteUn = document.querySelector("#titreRecetteUn");
+titreRecetteUn.textContent = recetteSaumon.nomRecette;
 
-const lister1 = document.getElementById("liste");
-console.log(lister1);
 
-r1.listeIngredient.forEach(element => {
+// Création d'un liste d'ingrédient grâce à la méthode forEach();
+
+const listeRecetteUn = document.querySelector("#listeRecetteUn");
+recetteSaumon.listeIngredient.forEach(element => {
     let li = document.createElement("li");
-    li.innerText = `${element.quantite * r1.nbPersonne}g ${element.nom}`;
-    lister1.appendChild(li);
+    li.innerText = `${element.quantite * recetteSaumon.nbPersonne}g ${element.nom}`;
+    listeRecetteUn.appendChild(li);
 });
 
-const personne = document.querySelector("#personneSaumon");
-personne.innerText = r1.nbPersonne + " " + "personne";
+const personneRecetteUn = document.querySelector("#personneRecetteUn");
+personneRecetteUn.innerText = recetteSaumon.nbPersonne + " " + "personne";
 
-const plusSaumon = document.querySelector("#plusSaumon");
-plusSaumon.innerText = "+";
+const plusRecetteUn = document.querySelector("#plusRecetteUn");
+plusRecetteUn.innerText = "+";
 
-plusSaumon.addEventListener("click", () => {
-    r1.nbPersonne++;
-    personne.innerText = `${r1.nbPersonne} personnes`;
-    lister1.innerText = "";
-    r1.listeIngredient.forEach(element => {
+//Ecouteur d'évènement pour changer le nombre de personnes et la quantité d'ingrédients
+
+plusRecetteUn.addEventListener("click", () => {
+    recetteSaumon.nbPersonne++;
+    personneRecetteUn.innerText = `${recetteSaumon.nbPersonne} personnes`;
+    listeRecetteUn.innerText = "";
+    recetteSaumon.listeIngredient.forEach(element => {
         let li = document.createElement("li");
-        li.innerText = `${element.quantite * r1.nbPersonne}g ${element.nom}`;
-        lister1.appendChild(li);
+        li.innerText = `${element.quantite * recetteSaumon.nbPersonne}g ${element.nom}`;
+        listeRecetteUn.appendChild(li);
     });
 });
 
-const moinsSaumon = document.querySelector("#moinsSaumon");
-moinsSaumon.innerText = "-";
+const moinsRecetteUn = document.querySelector("#moinsRecetteUn");
+moinsRecetteUn.innerText = "-";
 
-moinsSaumon.addEventListener("click", () => {
-    if(r1.nbPersonne > 1){
-        r1.nbPersonne--;
-        personne.innerText = `${r1.nbPersonne} personnes`;
-        lister1.innerText = "";
-        r1.listeIngredient.forEach(element => {
+moinsRecetteUn.addEventListener("click", () => {
+    if(recetteSaumon.nbPersonne > 1){
+        recetteSaumon.nbPersonne--;
+        personneRecetteUn.innerText = `${recetteSaumon.nbPersonne} personnes`;
+        listeRecetteUn.innerText = "";
+        recetteSaumon.listeIngredient.forEach(element => {
             let li = document.createElement("li");
-            li.innerText = `${element.quantite * r1.nbPersonne}g ${element.nom}`;
-            lister1.appendChild(li);
+            li.innerText = `${element.quantite * recetteSaumon.nbPersonne}g ${element.nom}`;
+            listeRecetteUn.appendChild(li);
         });
     }
-    if (r1.nbPersonne == 1) {
-        personne.innerText = `${r1.nbPersonne} personne`;
+    if (recetteSaumon.nbPersonne == 1) {
+        personneRecetteUn.innerText = `${recetteSaumon.nbPersonne} personne`;
+    };
+});
+
+// #region Recette Pickles
+
+let recettePickles = new Recette(
+    "Pickles d'Oignons",
+    [new Ingredient("Oignon Rouge", 200),
+    new Ingredient("Vinaigre de Cidre", 250),
+    new Ingredient("Vinaigre de Vin", 125),
+    new Ingredient("Sucre", 25),
+    new Ingredient("Sel et Poivre", 1)]
+);
+
+const titreRecetteDeux = document.querySelector("#titreRecetteDeux");
+titreRecetteDeux.innerText = recettePickles.nomRecette;
+
+const listeRecetteDeux = document.querySelector("#listeRecetteDeux");
+recettePickles.listeIngredient.forEach(element => {
+    let li = document.createElement("li");
+    li.innerText = `${element.quantite * recettePickles.nbPersonne}g ${element.nom}`;
+    listeRecetteDeux.appendChild(li); 
+});
+
+const personneRecetteDeux = document.querySelector("#personneRecetteDeux");
+personneRecetteDeux.innerText = recettePickles.nbPersonne + " " + "personne";
+
+const plusRecetteDeux = document.querySelector("#plusRecetteDeux");
+plusRecetteDeux.innerText = "+";
+
+plusRecetteDeux.addEventListener("click", () => {
+    recettePickles.nbPersonne++;
+    personneRecetteDeux.innerText = `${recettePickles.nbPersonne} personnes`;
+    listeRecetteDeux.innerText = "";
+    recettePickles.listeIngredient.forEach(element => {
+        let li = document.createElement("li");
+        li.innerText = `${element.quantite * recettePickles.nbPersonne}g ${element.nom}`;
+        listeRecetteDeux.appendChild(li);
+    });
+});
+
+const moinsRecetteDeux = document.querySelector("#moinsRecetteDeux");
+moinsRecetteDeux.innerText = "-";
+
+moinsRecetteDeux.addEventListener("click", () => {
+    if(recettePickles.nbPersonne > 1){
+        recettePickles.nbPersonne--;
+        personneRecetteDeux.innerText = `${recettePickles.nbPersonne} personnes`;
+        listeRecetteDeux.innerText = "";
+        recettePickles.listeIngredient.forEach(element => {
+            let li = document.createElement("li");
+            li.innerText = `${element.quantite * recettePickles.nbPersonne}g ${element.nom}`;
+            listeRecetteDeux.appendChild(li);
+        });
+    }
+    if (recettePickles.nbPersonne == 1) {
+        personneRecetteDeux.innerText = `${recettePickles.nbPersonne} personne`;
+    };
+});
+
+// #region Recette Riz 
+
+let recetteRiz = new Recette(
+    "Riz Vinaigré",
+    [new Ingredient("Riz Rond / Japonais", 130),
+    new Ingredient("Vinaigre de Riz", 20),
+    new Ingredient("Eau", 20),
+    new Ingredient("Sucre", 3),
+    new Ingredient("Sel", 2)]
+);
+
+const titreRecetteTrois = document.querySelector("#titreRecetteTrois");
+titreRecetteTrois.innerText = recetteRiz.nomRecette;
+
+const listeRecetteTrois = document.querySelector("#listeRecetteTrois");
+recetteRiz.listeIngredient.forEach(element => {
+    let li = document.createElement("li");
+    li.innerText = `${element.quantite * recetteRiz.nbPersonne}g ${element.nom}`;
+    listeRecetteTrois.appendChild(li); 
+});
+
+const personneRecetteTrois = document.querySelector("#personneRecetteTrois");
+personneRecetteTrois.innerText = recetteRiz.nbPersonne + " " + "personne";
+
+const plusRecetteTrois = document.querySelector("#plusRecetteTrois");
+plusRecetteTrois.innerText = "+";
+
+plusRecetteTrois.addEventListener("click", () => {
+    recetteRiz.nbPersonne++;
+    personneRecetteTrois.innerText = `${recetteRiz.nbPersonne} personnes`;
+    listeRecetteTrois.innerText = "";
+    recetteRiz.listeIngredient.forEach(element => {
+        let li = document.createElement("li");
+        li.innerText = `${element.quantite * recetteRiz.nbPersonne}g ${element.nom}`;
+        listeRecetteTrois.appendChild(li);
+    });
+});
+
+const moinsRecetteTrois = document.querySelector("#moinsRecetteTrois");
+moinsRecetteTrois.innerText = "-";
+
+moinsRecetteTrois.addEventListener("click", () => {
+    if(recetteRiz.nbPersonne > 1){
+        recetteRiz.nbPersonne--;
+        personneRecetteTrois.innerText = `${recetteRiz.nbPersonne} personnes`;
+        listeRecetteTrois.innerText = "";
+        recetteRiz.listeIngredient.forEach(element => {
+            let li = document.createElement("li");
+            li.innerText = `${element.quantite * recetteRiz.nbPersonne}g ${element.nom}`;
+            listeRecetteTrois.appendChild(li);
+        });
+    }
+    if (recetteRiz.nbPersonne == 1) {
+        personneRecetteTrois.innerText = `${recetteRiz.nbPersonne} personne`;
+    };
+});
+
+// #region Recette Teriyaki
+
+let recetteTeriyaki = new Recette(
+    "Sauce Teriyaki",
+    [new Ingredient("Sauce Soja", 125),
+    new Ingredient("Eau", 125),
+    new Ingredient("Maïzena", 10),
+    new Ingredient("Miel", 18),
+    new Ingredient("Mirin", 20)]
+);
+
+const titreRecetteQuatre = document.querySelector("#titreRecetteQuatre");
+titreRecetteQuatre.innerText = recetteTeriyaki.nomRecette;
+
+const listeRecetteQuatre = document.querySelector("#listeRecetteQuatre");
+recetteTeriyaki.listeIngredient.forEach(element => {
+    let li = document.createElement("li");
+    li.innerText = `${element.quantite * recetteTeriyaki.nbPersonne}g ${element.nom}`;
+    listeRecetteQuatre.appendChild(li); 
+});
+
+const personneRecetteQuatre = document.querySelector("#personneRecetteQuatre");
+personneRecetteQuatre.innerText = recetteTeriyaki.nbPersonne + " " + "personne";
+
+const plusRecetteQuatre = document.querySelector("#plusRecetteQuatre");
+plusRecetteQuatre.innerText = "+";
+
+plusRecetteQuatre.addEventListener("click", () => {
+    recetteTeriyaki.nbPersonne++;
+    personneRecetteQuatre.innerText = `${recetteTeriyaki.nbPersonne} personnes`;
+    listeRecetteQuatre.innerText = "";
+    recetteTeriyaki.listeIngredient.forEach(element => {
+        let li = document.createElement("li");
+        li.innerText = `${element.quantite * recetteTeriyaki.nbPersonne}g ${element.nom}`;
+        listeRecetteQuatre.appendChild(li);
+    });
+});
+
+const moinsRecetteQuatre = document.querySelector("#moinsRecetteQuatre");
+moinsRecetteQuatre.innerText = "-";
+
+moinsRecetteQuatre.addEventListener("click", () => {
+    if(recetteTeriyaki.nbPersonne > 1){
+        recetteTeriyaki.nbPersonne--;
+        personneRecetteQuatre.innerText = `${recetteTeriyaki.nbPersonne} personnes`;
+        listeRecetteQuatre.innerText = "";
+        recetteTeriyaki.listeIngredient.forEach(element => {
+            let li = document.createElement("li");
+            li.innerText = `${element.quantite * recetteTeriyaki.nbPersonne}g ${element.nom}`;
+            listeRecetteQuatre.appendChild(li);
+        });
+    }
+    if (recetteTeriyaki.nbPersonne == 1) {
+        personneRecetteQuatre.innerText = `${recetteTeriyaki.nbPersonne} personne`;
     };
 });
